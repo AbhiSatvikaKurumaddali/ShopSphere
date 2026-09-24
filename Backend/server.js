3e2088ada4e705f909425045b319ddf5fcc05b20
AbhiSatvikaKurumaddali <kurumaddali1201@gmail.com>
2026-09-24 09:37:25 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,7 @@ import express from "express";
 import http from "http";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import cors from "cors";
+
 import morgan from "morgan";
 
 import authRoutes from "./routes/authRoutes.js";
